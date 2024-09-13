114fd3bc845ff9c3fa434f555033e404d6b18a11
James Bracy <waratuman@gmail.com>
2024-09-13 16:00:29 -0400

    Rails 7.2 Updates

--- lib/active_record/connection_adapters/postgis_adapter.rb
+++ lib/active_record/connection_adapters/postgis_adapter.rb
@@ -7,34 +7,6 @@ require 'active_record/connection_adapters/postgis/database_statements'
 
 ActiveRecord::SchemaDumper.ignore_tables |= %w[geometry_columns spatial_ref_sys layer topology]
 module ActiveRecord
-  module ConnectionHandling # :nodoc:
-
-    # Establishes a connection to the database that's used by all Active Record objects
-    def postgis_connection(config)
-      conn_params = config.symbolize_keys
-
-      conn_params.delete_if { |_, v| v.nil? }
-
-      # Map ActiveRecords param names to PGs.
-      conn_params[:user] = conn_params.delete(:username) if conn_params[:username]
-      conn_params[:dbname] = conn_params.delete(:database) if conn_params[:database]
-
-      # Forward only valid config params to PG::Connection.connect.
-      valid_conn_param_keys = PG::Connection.conndefaults_hash.keys + [:requiressl]
-      conn_params.slice!(*valid_conn_param_keys)
-
-      conn = PG.connect(conn_params)
-      ConnectionAdapters::PostGISAdapter.new(conn, logger, conn_params, config)
-    rescue ::PG::Error => error
-      if error.message.include?("does not exist")
-        raise ActiveRecord::NoDatabaseError
-      else
-        raise
-      end
-    end
-
-  end
-
   module ConnectionAdapters
     class PostGISAdapter < PostgreSQLAdapter
       ADAPTER_NAME = 'PostGIS'.freeze
