ac3689681dc97e8e2801a4ecc2cce2dbbe4adb46
James Bracy <waratuman@gmail.com>
2024-09-13 15:38:51 -0400

    Rails 7.2 Updates

--- lib/postgis_adapter.rb
+++ lib/postgis_adapter.rb
@@ -1,1 +1,5 @@
 require 'postgis_adapter/railtie' if defined?(Rails)
+
+if ActiveRecord::ConnectionAdapters.respond_to?(:register)
+  ActiveRecord::ConnectionAdapters.register("postgis", "ActiveRecord::ConnectionAdapters::PostGISAdapter", "active_record/connection_adapters/postgis_adapter")
+end
--- test/cases/helper.rb
+++ test/cases/helper.rb
@@ -5,6 +5,7 @@ require "active_support"
 require "active_support/testing/autorun"
 
 ActiveRecord::Base.logger = ActiveSupport::Logger.new("debug.log", 0, 100 * 1024 * 1024)
+
 ActiveRecord::Base.configurations = ActiveRecord::DatabaseConfigurations.new({
   postgis_test: {
       adapter: "postgis",
