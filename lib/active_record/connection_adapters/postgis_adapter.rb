ceb5b191cc09ecf74551746b0ee99461bc7a219e
James Bracy <waratuman@gmail.com>
2025-01-03 12:18:29 -0500

    Load ActiveRecord prior to the adapter.

--- lib/active_record/connection_adapters/postgis_adapter.rb
+++ lib/active_record/connection_adapters/postgis_adapter.rb
@@ -1,3 +1,4 @@
+require 'active_record'
 require 'active_record/connection_adapters/postgresql_adapter'
 require 'active_record/connection_adapters/postgis/version'
 require 'active_record/connection_adapters/postgis/oid/geometry'
